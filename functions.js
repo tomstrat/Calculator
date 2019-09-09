93924ace41c8bc7c050d7932a62f358b59fba574
Tom Strat <tomstratfic@hotmail.com>
2019-09-09 20:38:53 +0100

    add styles and button event listeners

--- functions.js
+++ functions.js
@@ -1,3 +1,40 @@
+let display = document.getElementById("display");
+let error = document.getElementById("error");
+
+//Run this on startup
+//Event Listeners
+let numbers = document.querySelectorAll(".number")
+
+numbers.forEach(function(number){
+    number.addEventListener("click", function(e){
+        if(screenFull()){
+            displayError();
+        } else {
+            display.innerHTML += e.target.innerHTML;
+            return;
+        }
+    })
+})
+numbers.forEach(function(number){
+    number.addEventListener("transitionend", function(e){
+        if (e.propertyName !== 'transform') return;
+        e.target.classList.remove("numbersDown");
+    })
+})
+document.addEventListener("keydown", function(e){
+    for(i=0;i<numbers.length;i++){
+        if(e.key == numbers[i].innerHTML){
+            numbers[i].classList += " numbersDown";
+            if(screenFull()){
+                displayError();
+            } else {
+                display.innerHTML += numbers[i].innerHTML;
+                return;
+            }
+        } 
+    }
+})
+
 function add(a, b){
     return a + b;
 }
@@ -24,5 +61,17 @@ function operate(operator, a, b){
             return multiply(a,b);
         case "divide":
             return divide(a,b);
+    }
+}
+
+function screenFull(){
+    return display.innerHTML.length > 9 ? true : false;
+}
+
+function displayError(){
+    if(error.innerHTML.length > 0){
+        return;
+    } else {
+        error.innerHTML = "E"
     }
 }
