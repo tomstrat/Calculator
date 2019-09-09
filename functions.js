6f7eac8b81580f47331b87d4f8f509517a8b38d0
Tom Strat <tomstratfic@hotmail.com>
2019-09-09 22:05:23 +0100

    add functionality to buttons plus more math functions and event listeners

--- functions.js
+++ functions.js
@@ -1,9 +1,14 @@
 let display = document.getElementById("display");
 let error = document.getElementById("error");
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
 
 //Run this on startup
 //Event Listeners
 let numbers = document.querySelectorAll(".number")
+let operators = document.querySelectorAll(".operator");
+let equals = document.querySelector(".equals");
 
 numbers.forEach(function(number){
     number.addEventListener("click", function(e){
@@ -13,15 +18,36 @@ numbers.forEach(function(number){
             display.innerHTML += e.target.innerHTML;
             return;
         }
-    })
-})
-numbers.forEach(function(number){
+    });
     number.addEventListener("transitionend", function(e){
-        if (e.propertyName !== 'transform') return;
         e.target.classList.remove("numbersDown");
-    })
-})
+    });
+});
+
 document.addEventListener("keydown", function(e){
+    if(e.key == "="){
+        equals.classList += " equalsDown";
+        secondNumber = parseInt(display.innerHTML)
+        switch(operator){
+            case "+":
+                display.innerHTML = add(firstNumber, secondNumber);
+            case "-":
+                display.innerHTML = subtract(firstNumber, secondNumber);
+            case "x":
+                display.innerHTML = multiply(firstNumber, secondNumber);
+            case "/":
+                display.innerHTML = divide(firstNumber, secondNumber);
+        }
+    }
+    for(i=0;i<operators.length;i++){
+        if(e.key == operators[i].innerHTML){
+            operators[i].classList += " operatorsDown";
+            firstNumber = parseInt(display.innerHTML);
+            operator = e.target.innerHTML;
+            display.innerHTML = "";
+            return;
+        }
+    }
     for(i=0;i<numbers.length;i++){
         if(e.key == numbers[i].innerHTML){
             numbers[i].classList += " numbersDown";
@@ -33,8 +59,38 @@ document.addEventListener("keydown", function(e){
             }
         } 
     }
-})
+});
 
+operators.forEach(function(operator){
+    operator.addEventListener("click", function (e){
+        firstNumber = parseInt(display.innerHTML);
+        operator = e.target.innerHTML;
+        display.innerHTML = "";
+    });
+    operator.addEventListener("transitionend", function(e){
+        e.target.classList.remove("operatorsDown");
+    });
+});
+
+equals.addEventListener("click", function (e){
+    secondNumber = parseInt(display.innerHTML)
+    switch(operator){
+        case "+":
+            display.innerHTML = add(firstNumber, secondNumber);
+        case "-":
+            display.innerHTML = subtract(firstNumber, secondNumber);
+        case "x":
+            display.innerHTML = multiply(firstNumber, secondNumber);
+        case "/":
+            display.innerHTML = divide(firstNumber, secondNumber);
+    }
+});
+equals.addEventListener("transitionend", function(e){
+    e.target.classList.remove("equalsDown");
+});
+
+
+//Mathematic functions
 function add(a, b){
     return a + b;
 }
@@ -49,19 +105,6 @@ function multiply(a, b){
 
 function divide(a, b){
     return a / b;
-}
-
-function operate(operator, a, b){
-    switch(operator){
-        case "add":
-            return add(a,b);
-        case "subtract":
-            return subtract(a,b);
-        case "multiply":
-            return multiply(a,b);
-        case "divide":
-            return divide(a,b);
-    }
 }
 
 function screenFull(){
